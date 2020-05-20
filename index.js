814387dcc434cb9a12bb1eda9c39ba3de91e3436
rjmacarthy <richardmacarthy@hotmail.com>
2020-05-20 10:47:19 +0100

    test

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 require('dotenv').config()
 const express = require('express')
 const crypto = require('crypto')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT
+const SECRET = process.env.SECRET
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app
   .post('/deploy', (req, res) => {
+    const payload = req.body
     const signature = `sha1=${crypto
       .createHmac('sha1', SECRET)
-      .update(chunk)
+      .update(JSON.stringify(payload))
       .digest('hex')}`
-
     const event = req.headers['x-github-event']
-
-    const isAllowed = req.headers['x-hub-signature'] === signature
-
-    console.log(signature)
-
-    const body = JSON.parse(chunk)
-
-    console.log(body.ref, isAllowed)
-
-    const isMaster = body.ref === 'refs/heads/master'
-
-    if (isAllowed && isMaster && event === 'push') {
-      console.log('deploy')
+    const allowed = req.headers['x-hub-signature'] === signature
+    const master = payload.ref === 'refs/heads/master'
+    if (allowed && isMaster && event === 'push') {
+      return res.json({ allowed, master })
     }
-
-    res.json(isAllowed, isMaster)
+    res.json({ allowed, master })
   })
   .get('/deploy', (_, res) => {
     res.json('eosui-deployer')
