6718d44cb6f6fdf408100147635120a20891268e
rjmacarthy <richardmacarthy@hotmail.com>
2020-05-20 10:31:25 +0100

    dotenv and index route

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
+require('dotenv').config()
 const express = require('express')
 const crypto = require('crypto')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
-app.post('/', (req, res) => {
-  const signature = `sha1=${crypto
-    .createHmac('sha1', SECRET)
-    .update(chunk)
-    .digest('hex')}`
+app
+  .post('/', (req, res) => {
+    const signature = `sha1=${crypto
+      .createHmac('sha1', SECRET)
+      .update(chunk)
+      .digest('hex')}`
 
-  const event = req.headers['x-github-event']
+    const event = req.headers['x-github-event']
 
-  const isAllowed = req.headers['x-hub-signature'] === signature
+    const isAllowed = req.headers['x-hub-signature'] === signature
 
-  console.log(signature)
+    console.log(signature)
 
-  const body = JSON.parse(chunk)
+    const body = JSON.parse(chunk)
 
-  console.log(body.ref, isAllowed)
+    console.log(body.ref, isAllowed)
 
-  const isMaster = body.ref === 'refs/heads/master'
+    const isMaster = body.ref === 'refs/heads/master'
 
-  if (isAllowed && isMaster && event === 'push') {
-    console.log('deploy')
-  }
-}).get((req, res)=>{
+    if (isAllowed && isMaster && event === 'push') {
+      console.log('deploy')
+    }
 
-})
+    res.json(isAllowed, isMaster)
+  })
+  .get('/', (_, res) => {
+    res.json('eosui-deployer')
+  })
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`),
