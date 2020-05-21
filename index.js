58c471801ba0f59e6bb26cf72a5d1e3c66985a55
rjmacarthy <richardmacarthy@hotmail.com>
2020-05-21 14:53:12 +0100

    add helmet

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const helmet = require('helmet')
+
 const { router } = require('./router')
 const logger = require('./logger')
 
 logger.init()
 const port = process.env.PORT
 const app = express()
+app.use(helmet())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
