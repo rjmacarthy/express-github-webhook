392b7a6f829115fab64dfb661fb04df79578240a
rjmacarthy <richardmacarthy@hotmail.com>
2020-06-01 09:02:52 +0100

    more tests

--- test/test.js
+++ test/test.js
@@ -2,13 +2,22 @@ const sinon = require('sinon')
 const { beforeEach, afterEach, describe, it } = require('mocha')
 const { expect } = require('chai')
 const { createRequest, createResponse } = require('node-mocks-http')
-const { index, deploy } = require('./controllers')
 const cp = require('child_process')
+const fs = require('fs')
 
-describe('controllers', () => {
+const { index, deploy } = require('../lib/controllers')
+const logger = require('../lib/logger')
+const utils = require('../lib/utils')
+
+describe('controllers suite', () => {
   let req
   let res
   let exec
+
+  before(() => {
+    fs.unlinkSync(logger.logPath)
+    fs.rmdirSync(logger.logFolder)
+  })
 
   beforeEach(() => {
     res = createResponse()
@@ -16,13 +25,14 @@ describe('controllers', () => {
     exec = sinon.stub(cp, 'exec')
     res.sendFile = sinon.spy()
     res.end = sinon.spy()
+    
   })
 
   afterEach(() => {
     exec.restore()
   })
 
-  it('Can render the index page', async () => {
+  it('can render the index page', async () => {
     index(req, res)
     expect(res.sendFile.calledOnce).to.equal(true)
   })
@@ -40,5 +50,27 @@ describe('controllers', () => {
     deploy(req, res)
     expect(res.end.calledOnce).to.equal(true)
     expect(exec.calledOnce).to.equal(false)
+  })
+})
+
+describe('logger', () => {
+  it('can init the logger', () => {
+    const mkdirSync = sinon.stub(fs, 'mkdirSync')
+    const existsSync = sinon.stub(fs, 'existsSync')
+    const writeFileSync = sinon.stub(fs, 'writeFileSync')
+    logger.init()
+    expect(mkdirSync.calledOnce).to.equal(false)
+    expect(existsSync.calledOnce).to.equal(false)
+    expect(writeFileSync.calledOnce).to.equal(false)
+  })
+})
+
+describe('utils', () => {
+  it('can execute and write a log', () => {
+    utils.handler(null, 'test', null)
+  })
+  
+  it('can execute and write an error', () => {
+    utils.handler(null, null, 'error')
   })
 })
